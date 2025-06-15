0c672f89f0140d66d082b90aaacc3c29fd2856e1
Roman R <surgutyan@yandex.ru>
2025-06-15 05:23:48 +0500

    Add headless mode support for CI/CD

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,8 @@ logger = logging.getLogger("qa")
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="https://berpress.github.io/selenium-login-demo/",
                      help="url")
+    parser.addoption("--headless", action="store_true",
+                     help="url")
 
 """
 @pytest.fixture(scope="session") # scope="session" позволяет ускорить выполнение тестов
@@ -35,9 +37,12 @@ def loginform_page(request):
 @pytest.fixture(scope="session") # scope="session" позволяет ускорить выполнение тестов
 def loginform_page_2(request):
     url = request.config.getoption('--url')
-    logger.info(f'Start app on url {url}')
+    is_headless = request.config.getoption('--headless')
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
+    if is_headless:
+        chrome_options.add_argument("--headless=new")
+    logger.info(f'Start app on url {url}, headless is {is_headless}')
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     loginform_page2 = LoginformPage2(driver)
